5e2c351c1acc02df8598d599f80a0bc83a56aa1d
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2020-09-17 14:53:41 +0300

    Modify text processor

--- src/raw_text_processor/main.py
+++ src/raw_text_processor/main.py
@@ -8,14 +8,20 @@ if __name__ == '__main__':
         with open(os.path.join(PROCESSED_DIR_PATH, filename), 'w') as raw_file:
             with open(os.path.join(RAW_DIR_PATH, filename), 'r', encoding='utf-8', errors='ignore') as processed_file:
                 for line in processed_file:
-                    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=[.?!])(\s|")', line)
+                    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=[.?!])', line)
                     for sentence in sentences:
                         sentence = sentence.strip(' .?!"\'\n')
                         sentence = sentence.replace('..', '')
                         sentence = sentence.replace('--', '')
+                        if len(sentence) < 2:
+                            continue
+                        sentence = sentence[0].lower() + sentence[1:]
                         sentence = re.sub(r'[^A-Za-z\' .-]', '', sentence)
                         sentence = re.sub(r' +', ' ', sentence)
                         words = sentence.split(' ')
                         for word in words:
+                            word = word.strip('\' -')
                             if word != '':
+                                if len(word) > 1 and not re.search(r'[A-Z]+[a-z]+$', word):
+                                    word = word.lower()
                                 raw_file.write(f'{word}\n')
