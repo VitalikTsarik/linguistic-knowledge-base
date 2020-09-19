a1bb02013294fd4d0858ead5c9a6550877c6d649
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2020-09-19 16:39:29 +0300

    Add "add text" and "close" actions, integrate raw text processing into app

--- src/dictionary/helpers.py
+++ src/dictionary/helpers.py
@@ -1,6 +1,32 @@
 import os
+import re
 
 from constants import PROCESSED_DIR_PATH
+
+
+def processRawTexts(filenames):
+    result = []
+    for filename in filenames:
+        with open(filename, 'r', encoding='utf-8', errors='ignore') as processed_file:
+            for line in processed_file:
+                sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=[.?!])', line)
+                for sentence in sentences:
+                    sentence = sentence.strip(' .?!"\'\n')
+                    sentence = sentence.replace('..', '')
+                    sentence = sentence.replace('--', '')
+                    if len(sentence) < 2:
+                        continue
+                    sentence = sentence[0].lower() + sentence[1:]
+                    sentence = re.sub(r'[^A-Za-z\' .-]', '', sentence)
+                    sentence = re.sub(r' +', ' ', sentence)
+                    words = sentence.split(' ')
+                    for word in words:
+                        word = word.strip('\' -')
+                        if word != '':
+                            if len(word) > 1 and not re.search(r'[A-Z]+[a-z]+$', word):
+                                word = word.lower()
+                            result.append(word)
+    return result
 
 
 def read_processed_texts():
@@ -12,3 +38,19 @@ def read_processed_texts():
                 data[word] = data.setdefault(word, 0) + 1
 
     return data
+
+
+def readTexts(filenames):
+    words = processRawTexts(filenames)
+    data = {}
+    for word in words:
+        data[word] = data.setdefault(word, 0) + 1
+
+    return data
+
+
+def mergeDicts(a, b):
+    result = a.copy()
+    for key, value in b.items():
+        result[key] = result.setdefault(key, 0) + value
+    return result
--- src/gui/app.py
+++ src/gui/app.py
@@ -1,20 +1,38 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 
 from dictionary.dictionary_model import DictionaryModel
-from dictionary.helpers import read_processed_texts
+from dictionary.helpers import readTexts, mergeDicts
 from gui.gen.main_window import Ui_MainWindow
 
 
 class App(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(App, self).__init__(parent)
+        self.__dictionary = {}
         self.setupUi(self)
-        dictionary = read_processed_texts()
-        records = [[key, value] for key, value in dictionary.items()]
-        self.model = DictionaryModel(records=records)
-        self.tableView.setModel(self.model)
+        self.__initUI()
+
+    def __initUI(self):
+        self.actionAddText.triggered.connect(self.__onAddText)
+        self.actionClose.triggered.connect(self.__onClose)
+
+    def __updateTable(self):
+        records = [[key, value] for key, value in self.__dictionary.items()]
+        self.tableView.setModel(DictionaryModel(records=records))
+
+    def __onAddText(self):
+        filenames, _ = QFileDialog.getOpenFileNames(self, 'Open file', '', 'Text Files (*.txt)')
+        if len(filenames):
+            print(filenames)
+            textsData = readTexts(filenames)
+            self.__dictionary = mergeDicts(self.__dictionary, textsData)
+            self.__updateTable()
+
+    def __onClose(self):
+        self.__dictionary = {}
+        self.__updateTable()
 
 
 def main():
